86ba14bc437bf0c7a5aaca8e242b22e46765842f
Mahmud <rainbowmahmud@gmail.com>
2022-02-19 15:50:10 +0600

    word selection functionality added.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
+const textToType =
+  "In the case of UI events, this is important for security reasons, as it prevents scripts from simulating user actions that interact with the browser itself.";
+
 // DOM
-const testText = document.getElementById("test-text").innerText;
+const testText = document.getElementById("test-text");
 const inputText = document.getElementById("input-text");
 const specialChar = [
   "semicolon",
@@ -15,20 +18,35 @@ const specialChar = [
   "quote",
   "space",
 ];
+
+const wordsArr = textToType.split(" ");
+
+document.addEventListener("DOMContentLoaded", () => {
+  for (let word of wordsArr) {
+    testText.innerHTML += `<span>${word} </span>`;
+  }
+});
+
 // start inputting text
 document.getElementById("start").addEventListener("click", (e) => {
   e.target.innerText = "Stop";
-
   inputText.focus();
 
-  startTyping();
+  wordsNode = testText.children;
+  wordsNode[0].classList.add("ready");
+
+  startTyping(wordsNode);
 });
 
 // start typing
-function startTyping() {
-  let charIndex = 0;
+function startTyping(wordsNode) {
+  let charIndex = 0,
+    wordIndex = 0,
+    word = "";
 
-  window.addEventListener("keydown", (e) => {
+  inputText.addEventListener("keydown", (e) => {
+    // e.preventDefault();
+
     // Alpha Numeric key
     const alphaNumericKey =
       e.code.toLowerCase().startsWith("key") ||
@@ -36,8 +54,19 @@ function startTyping() {
       e.code.toLowerCase("numpad").startsWith("key") ||
       specialChar.includes(e.code.toLocaleLowerCase());
 
+    // word index
+    if ("Space" == e.code) {
+      wordsNode[wordIndex].classList.remove("ready");
+
+      wordIndex++;
+      // console.log("word", wordsNode);
+      // add ready class to next word
+      wordsNode[wordIndex].classList.add("ready");
+    }
+
+    // alpha numeric key
     if (alphaNumericKey) {
-      // console.log(`${e.key} == ${testText[charIndex]}`);
+      // console.log(`${e.key} == ${textToType[charIndex]}`);
       charIndex++;
     }
 
